e1f132b47b0785656b59f9c984179d9ec3a73a37
Dredyx <51200743+Dredyx@users.noreply.github.com>
2019-05-31 10:26:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 prompt("FUDERS MUST GO IN HELL");
 
-if (confirm('Are you sure you want them to rot in Hell ?')) {
+if (confirm('Are you sure you want them to rot in Hell ? If yes click the FUD button')) {
     // Save it!
 } else {
     // Do nothing!
